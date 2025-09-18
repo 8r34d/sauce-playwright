ccb2781e921abcce9be26528f24af73e20abf0d3
Dean Spencer <dean_spencer@yahoo.co.uk>
2025-09-18 02:56:29 +0100

    add expect export to example fixture

--- fixtures/example-fixture.ts
+++ fixtures/example-fixture.ts
@@ -18,56 +18,33 @@ type CheckoutFlowFixtures = {
 export const test = base.extend<CheckoutFlowFixtures>({
   cartPage: async ({ page }, use) => {
     const cartPage = new CartPage(page);
-    // add fixture steps
-    // ...
-    // use the fixture value in the test
+
     await use(cartPage);
-    // add cleanup (if applcable)
-    // ...
   },
   checkoutCompletePage: async ({ page }, use) => {
     const checkoutCompletePage = new CheckoutCompletePage(page);
-    // add fixture steps
-    // ...
-    // use the fixture value in the test
+
     await use(checkoutCompletePage);
-    // add cleanup (if applcable)
-    // ...
   },
   checkoutOverviewPage: async ({ page }, use) => {
     const checkoutOverviewPage = new CheckoutOverviewPage(page);
-    // add fixture steps
-    // ...
-    // use the fixture value in the test
+
     await use(checkoutOverviewPage);
-    // add cleanup (if applcable)
-    // ...
   },
   checkoutYourInformationPage: async ({ page }, use) => {
     const checkoutYourInformationPage = new CheckoutYourInformationPage(page);
-    // add fixture steps
-    // ...
-    // use the fixture value in the test
+
     await use(checkoutYourInformationPage);
-    // add cleanup (if applcable)
-    // ...
   },
   inventoryItemPage: async ({ page }, use) => {
     const inventoryItemPage = new InventoryItemPage(page);
-    // add fixture steps
-    // ...
-    // use the fixture value in the test
+
     await use(inventoryItemPage);
-    // add cleanup (if applcable)
-    // ...
   },
   inventoryPage: async ({ page }, use) => {
     const inventoryPage = new InventoryPage(page);
-    // add fixture steps
-    // ...
-    // use the fixture value in the test
+
     await use(inventoryPage);
-    // add cleanup (if applcable)
-    // ...
   },
 });
+export { expect } from "@playwright/test";

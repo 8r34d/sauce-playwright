e2795f2541e413c173250eac16cc22152ec4ef1e
Dean Spencer <dean_spencer@yahoo.co.uk>
2025-10-25 15:19:18 +0100

    add staging env

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,9 @@ import { defineConfig, devices } from "@playwright/test";
  */
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env.local") });
+dotenv.config({
+  path: path.resolve(__dirname, `.env.${process.env.TEST_ENV || "local"}`),
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://www.saucedemo.com",
+    baseURL: process.env.BASE_URL ?? "https://www.saucedemo.com",
 
     // https://playwright.dev/docs/api/class-testoptions#test-options-screenshot
     screenshot: { mode: "only-on-failure", fullPage: true },
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,11 +3,14 @@ import path from "path";
 
 const authFile = path.join(__dirname, "../playwright/.auth/user.json");
 
+const { USERNAME, PASSWORD, BASE_URL } = process.env;
+
 setup("authenticate", async ({ page }) => {
+  console.log(`Base URL: ${BASE_URL}`);
   await page.goto("/");
   await expect(page).toHaveTitle(/Swag Labs/);
-  await page.getByTestId("username").fill(process.env.USERNAME!);
-  await page.getByTestId("password").fill(process.env.PASSWORD!);
+  await page.getByTestId("username").fill(USERNAME!);
+  await page.getByTestId("password").fill(PASSWORD!);
   await page.getByTestId("login-button").click();
   await page.context().storageState({ path: authFile });
 });
